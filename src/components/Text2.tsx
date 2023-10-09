c5ac3e70855746446d52315c57909d748d8992eb
Pheewww <umangsingh67899876@gmail.com>
2023-10-09 23:46:14 +0530

    Footer and Why us segment

--- src/components/Text2.tsx
+++ src/components/Text2.tsx
@@ -1,25 +1,67 @@
-import React from 'react';
+import React from 'react'
+import { DollarSign, Zap, Moon, Filter } from 'lucide-react'
 
-const Text2 = () => {
+export function Text2() {
   return (
-    <div>
-      <div className='flex flex-col sm:flex-row justify-around sm:w-full w-[80vw] p-6 sm:p-10 mt-[2%] mb-16'>
-        <div className='w-full sm:w-[20%]'>
-          <h2 className='text-3xl font-bold text-center mb-5'>Be the best you with EQ</h2>
-        </div>
-        <div className='w-full sm:w-[22%] text-base text-gray-700 font-medium mb-5'>
-          <p className='text-lg'>
-            People with high emotional intelligence (EQ) live more fulfilled lives. They tend to be happier and have healthier relationships.
+    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-xl text-center">
+        <div className="mx-auto inline-flex rounded-full bg-gray-100 px-4 py-1.5">
+          <p className="text-xs font-semibold uppercase tracking-widest text-black">
+            100+ Tailored Websites
           </p>
         </div>
-        <div className='w-full sm:w-[22%] text-gray-700 font-medium'>
-          <p className='text-lg'>
-            They are more successful in their pursuits and make for inspiring leaders. According to science, they earn $29k more per year.
+        <h2 className="mt-6 text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl">
+          DevDynasty helps you build beautiful website
+        </h2>
+        <p className="mt-4 text-base leading-relaxed text-gray-600">
+          Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia
+          consequat duis enim velit mollit.
+        </p>
+      </div>
+      <div className="mt-12 grid grid-cols-1 gap-y-8 text-center sm:grid-cols-2 sm:gap-12 lg:grid-cols-4">
+        <div>
+          <div className="mx-auto flex h-20 w-20 items-center justify-center rounded-full bg-gray-100">
+            <DollarSign className="h-9 w-9 text-gray-700" />
+          </div>
+          <h3 className="mt-8 text-lg font-semibold text-black">Secured Payments</h3>
+          <p className="mt-4 text-sm text-gray-600">
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia
+            consequat duis enim velit mollit.
+          </p>
+        </div>
+        <div>
+          <div className="mx-auto flex h-20 w-20 items-center justify-center rounded-full bg-gray-100">
+            <Zap className="h-9 w-9 text-gray-700" />
+          </div>
+          <h3 className="mt-8 text-lg font-semibold text-black">Fast & Easy to Load</h3>
+          <p className="mt-4 text-sm text-gray-600">
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia
+            consequat duis enim velit mollit.
+          </p>
+        </div>
+        <div>
+          <div className="mx-auto flex h-20 w-20 items-center justify-center rounded-full bg-gray-100">
+            <Moon className="h-9 w-9 text-gray-700" />
+          </div>
+          <h3 className="mt-8 text-lg font-semibold text-black">Light & Dark Version</h3>
+          <p className="mt-4 text-sm text-gray-600">
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia
+            consequat duis enim velit mollit.
+          </p>
+        </div>
+        <div>
+          <div className="mx-auto flex h-20 w-20 items-center justify-center rounded-full bg-gray-100">
+            <Filter className="h-9 w-9 text-gray-700" />
+          </div>
+          <h3 className="mt-8 text-lg font-semibold text-black">Filter Blocks</h3>
+          <p className="mt-4 text-sm text-gray-600">
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia
+            consequat duis enim velit mollit.
           </p>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
 export default Text2;
