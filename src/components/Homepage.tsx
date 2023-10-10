c4ff1a98f1887e5975259ce2e39b186915da1b0a
Ayush Kumar <94730528+Akayush-17@users.noreply.github.com>
2023-10-10 23:29:56 +0530

    Update Homepage.tsx

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -5,13 +5,14 @@ import Landing from './Landing';
 import Services from './Services'
 import Pricing from './Pricing';
 import Footer from './Footer';
+import Whyus from './Text2';
 
 const Homepage = () => {
   return (
     <>
       <Landing/>
       <Services/>
-      
+      <Whyus/>
       <Projects/>
       <RecentWork/>
       <Pricing/>
